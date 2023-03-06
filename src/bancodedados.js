88be99bd96f7e97c9fcc5bb7a4d57f858eac7a00
Lilian Carvalho <lilianoak.dev@gmail.com>
2023-03-06 18:27:36 -0300

    Instalações concluídas

---
+++ src/bancodedados.js
@@ -0,0 +1,12 @@
+module.exports = {
+    banco: {
+        nome: 'MoneyLink Bank',
+        numero: '123',
+        agencia: '0001',
+        senha: 'MoneyLink123Bank'
+    },
+    contas: [],
+    saques: [],
+    depositos: [],
+    transferencias: []
+}
