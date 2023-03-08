5abd401ce6dc1e4190d501e6b7d8e20b29bc4b6b
Lilian Carvalho <lilianoak.dev@gmail.com>
2023-03-08 09:39:51 -0300

    Depositar e Sacar ok

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const contas = require('./controladores/contas')
+const contas = require('./controladores/contas');
+const transacoes = require('./controladores/transacoes')
 
 const rotas = express();
 
@@ -7,5 +8,8 @@ rotas.get('/contas', contas.listarContas);
 rotas.post('/contas', contas.criarConta);
 rotas.put('/contas/:numeroConta/usuario', contas.atualizarUsuarioConta);
 rotas.delete('/contas/:numeroConta', contas.excluirConta);
+
+rotas.post('/transacoes/depositar', transacoes.depositar);
+rotas.post('/transacoes/sacar', transacoes.sacar);
 
 module.exports = rotas;
